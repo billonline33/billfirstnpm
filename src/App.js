10b72e5a767efa8e62366509cd16a89e97b00453
Bill Huang <billonline33@hotmail.com>
2020-11-03 16:18:53 +1100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import { TextInput } from 'simple-component-library-bill';
+import { XPOHeader } from 'xpo-header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      TextInput
       <TextInput label="Email Address" placeholder="name@example.com" />
+      <XPOHeader label="Email Address 2" placeholder="name@example2.com" />
     </div>
   );
 }
