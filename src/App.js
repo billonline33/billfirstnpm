167f4eaa9a4d259731935fd1656cc523bec0e161
Bill Huang <billonline33@hotmail.com>
2020-11-09 16:45:32 +1100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import { TextInput } from 'simple-component-library-bill';
 import { XPOHeader } from 'xpo-header';
+// import { XPOHeader } from 'gokul-react-package';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      TextInput
-      <TextInput label="Email Address" placeholder="name@example.com" />
+      {/* TextInput
+      <TextInput label="Email Address" placeholder="name@example.com" /> */}
       <XPOHeader label="Email Address 2" placeholder="name@example2.com" />
     </div>
   );
